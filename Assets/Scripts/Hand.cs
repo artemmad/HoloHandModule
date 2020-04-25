302ca004f34cfcb1f2c25a62151916e639decac4
Artem <artemmakarevich1997@gmail.com>
2020-04-26 01:21:00 +0300

    Add hand to scene replication
    Project have been mooved to 2018 stable version

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class Hand : MonoBehaviour
+{
+    private Vector3 userHandPos;
+    private Quaternion userHandRot;
+
+    private void Awake()
+    {
+        InteractionManager.InteractionSourceUpdated += HandPosition;
+    }
+
+    private void HandPosition(InteractionSourceUpdatedEventArgs interactionSourceUpdatedEventArgs)
+    {
+        interactionSourceUpdatedEventArgs.state.sourcePose.TryGetPosition(out userHandPos);
+        interactionSourceUpdatedEventArgs.state.sourcePose.TryGetRotation(out userHandRot);
+    }
+
+    private void Update()
+    {
+        transform.position = userHandPos;
+        transform.rotation = userHandRot;
+    }
+
+}
